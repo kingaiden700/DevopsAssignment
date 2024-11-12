4461c6b0646d41de4caea843dbac5065d7316e6e
Aiden <kingaiden700@gmail.com>
2024-11-12 07:20:41 +0530

    First Commit

---
+++ devops.java
@@ -0,0 +1,10 @@
+class devops
+{
+public static void main(String args[])
+{
+System.out.println("Nihal 532");
+System.out.println("Saiteja 547");
+System.out.println("Namrata 527");
+System.out.println("Ramya 542");
+}
+};
